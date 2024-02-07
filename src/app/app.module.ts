8c88f486ddb00ef7e8af025137bca6fd061fa98c
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-02-07 15:37:17 +0330

    add a directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './test/Directive/highlight.directive';
 
 @NgModule({
   declarations: [AppComponent, TestComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HighlightDirective],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
---
+++ src/app/test/Directive/highlight.directive.ts
@@ -0,0 +1,10 @@
+import { Directive, ElementRef } from '@angular/core';
+@Directive({
+  standalone: true,
+  selector: '[appHighlight]',
+})
+export class HighlightDirective {
+  constructor(private el: ElementRef) {
+    this.el.nativeElement.style.backgroundColor = 'yellow';
+  }
+}
