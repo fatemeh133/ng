a2515d07cce5e5a972e525e5ba668f34b7f31c74
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-02-18 16:55:02 +0330

    404 error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { Test2Component } from './test2/test2.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/Test2', pathMatch: 'full' },
   { path: 'Test2', component: Test2Component },
   { path: 'Test', component: TestComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,10 @@ import { HighlightDirective } from './test/Directive/highlight.directive';
 import { RendererHilightDirective } from './test/Directive/renderer-hilight.directive';
 import { HideAfterDirective } from './test/Directive/hide-after.directive';
 import { Test2Component } from './test2/test2.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, TestComponent, RendererHilightDirective, HideAfterDirective, Test2Component],
+  declarations: [AppComponent, TestComponent, RendererHilightDirective, HideAfterDirective, Test2Component, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HighlightDirective],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
